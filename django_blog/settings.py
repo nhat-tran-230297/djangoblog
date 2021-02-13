fa2189738de4a0cffec05ac70468e63a1c12a6f4
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-02-14 00:01:25 +0200

    configure heroku_django

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,3 +159,7 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_REGION_NAME = 'eu-north-1'
+
+
+# automatically configure setting for Heroku Postgres db
+django_heroku.settings(locals())
